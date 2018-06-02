d8bedcdbf24dc9a207bff3f6a7fa73d73989b926
Skye <xkhsky@163.com>
2018-06-02 13:57:57 +0800

    fixL make metapath

--- core/src/main/java/hdu/metapath/MakeMetaPath.java
+++ core/src/main/java/hdu/metapath/MakeMetaPath.java
@@ -2,6 +2,8 @@ package hdu.metapath;
 
 import net.librec.math.structure.SparseMatrix;
 
+import java.io.IOException;
+
 /**
  * @Author: Skye
  * @Date: 21:37 2018/5/31
@@ -9,6 +11,6 @@ import net.librec.math.structure.SparseMatrix;
  */
 public interface  MakeMetaPath {
 
-    public void processData();
+    public void processData() throws IOException;
     public SparseMatrix getPreferenceMatrix();
 }
--- core/src/main/java/hdu/metapath/MakeUPUP.java
+++ core/src/main/java/hdu/metapath/MakeUPUP.java
@@ -1,6 +1,14 @@
 package hdu.metapath;
 
+import com.google.common.collect.*;
+import hdu.util.FileUtil;
+import net.librec.data.convertor.TextDataConvertor;
 import net.librec.math.structure.SparseMatrix;
+import net.librec.util.StringUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
 
 /**
  * @Author: Skye
@@ -9,15 +17,77 @@ import net.librec.math.structure.SparseMatrix;
  *
  */
 public class MakeUPUP implements MakeMetaPath{
+    private static final Log LOG = LogFactory.getLog(MakeUPUP.class);
+
+    private BiMap<String, Integer> userIds, itemIds;
     private SparseMatrix UPMatrix;
-
-    @Override
-    public void processData() {
-
+    private String inputDataPath;
+    private SparseMatrix preferenceMatrix;
+    public MakeUPUP(String inputDataPath){
+        this.inputDataPath = inputDataPath;
     }
+    @Override
+    public void processData() throws IOException{
+        readData(inputDataPath);
+    }
+    private void readData(String inputDataPath) throws IOException {
+        LOG.info(String.format("Dataset: %s", StringUtil.last(inputDataPath, 38)));
+        // Table {row-id, col-id, count}
+        Table<Integer, Integer, Integer> dataTable = HashBasedTable.create();
+        // Map {col-id, multiple row-id}: used to fast build a rating matrix
+        Multimap<Integer, Integer> colMap = HashMultimap.create();
+        if (this.userIds == null) {
+            this.userIds = HashBiMap.create();
+        }
+        if (this.itemIds == null) {
+            this.itemIds = HashBiMap.create();
+        }
+        String[] content = FileUtil.read(inputDataPath,null);
+        for (int i = 0; i < content.length; i++) {
+            String line = content[i];
+            String[] data = line.trim().split("[ \t,]+");
+            String user = data[0];
+            String item = data[1];
+            int count = Integer.parseInt(data[2]);
+            int row = userIds.containsKey(user) ? userIds.get(user) : userIds.size();
+            userIds.put(user, row);
 
+            int col = itemIds.containsKey(item) ? itemIds.get(item) : itemIds.size();
+            itemIds.put(item, col);
+
+            dataTable.put(row, col, count);
+            colMap.put(col, row);
+        }
+        int numRows = numUsers(), numCols = numItems();
+        // build counting matrix
+        UPMatrix = new SparseMatrix(numRows, numCols, dataTable, colMap);
+
+        // release memory of data table
+        dataTable = null;
+    }
+    public processPreferenceMatrix()
     @Override
     public SparseMatrix getPreferenceMatrix() {
         return null;
+    }
+
+    public int numUsers() {
+        return userIds.size();
+    }
+
+    public int numItems() {
+        return itemIds.size();
+    }
+
+    public BiMap<String, Integer> getUserIds() {
+        return userIds;
+    }
+
+    public BiMap<String, Integer> getItemIds() {
+        return itemIds;
+    }
+
+    public SparseMatrix getUPMatrix() {
+        return UPMatrix;
     }
 }
